4f641236f624c2a5347083c4f19bfa1eb8bd98e5
2119744 <2119744@LTIN574508.cts.com>
2024-08-26 15:50:58 +0530

    0011

--- Proj001/src/com/nt/main/A1.java
+++ Proj001/src/com/nt/main/A1.java
@@ -5,6 +5,7 @@ public class A1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("A1.main() 001`");
+		System.out.println("A1.main() 0011");
 	}
 
 }
